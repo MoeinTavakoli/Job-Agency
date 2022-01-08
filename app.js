45ecb7ad4cd0af4e73872ced8e46b1f48fc600cb
moeinTavakoli <mtawakoli2283@gmail.com>
2022-01-08 15:22:19 +0330

    add create job and set to queue

--- app.js
+++ app.js
@@ -8,11 +8,32 @@ const bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-
 require("./db/connection")
+
+const { createJobQueue } = require("./db/user")
+
 
 app.use("/user", require("./router/user"))
 
+
+app.post("/user/dashboard/create", async (req, res) => {
+    try {
+        const { title, description } = req.body
+        const user_id = 37
+        const result = await createJobQueue(user_id, title, description)
+        if (result == false) {
+            return res.json({ success: false, error: "user id not found !" })
+        }
+        if (result.rowCount == 1) {
+            res.json({ success: true, message: "job create and wait for admin to accept ..." })
+        } else {
+            res.json({ success: false, message: "job didnt create" })
+        }
+    } catch (error) {
+        console.log(error);
+        res.send(error)
+    }
+})
 
 app.listen(port = 3000, () => {
     console.log(`Example app listening at http://localhost:${port}`)
--- db/user/index.js
+++ db/user/index.js
@@ -3,7 +3,7 @@ const client = require('../connection')
 async function signupDB(name, skill, education, username, password) {
   try {
     const result = await client.query(
-      'INSERT INTO public.user(name, skill, education, username, password)VALUES ( $1 , $2 , $3 , $4 , $5  );',
+      'INSERT INTO users(name, skill, education, username, password)VALUES ( $1 , $2 , $3 , $4 , $5  );',
       [name, skill, education, username, password]
     )
     return result.rows
@@ -15,7 +15,7 @@ async function signupDB(name, skill, education, username, password) {
 async function loginDB(username, password) {
   try {
     const result = await client.query(
-      'SELECT * FROM "user" WHERE username = $1 AND password = $2', [username, password])
+      'SELECT * FROM users WHERE username = $1 AND password = $2', [username, password])
     return result.rows
   }
   catch (error) {
@@ -24,8 +24,24 @@ async function loginDB(username, password) {
 }
 
 
+async function createJobQueue(user_id, title, description) {
+  try {
+    const result = await client.query("INSERT INTO public.queue(user_id, title, description) VALUES( $1 , $2 , $3 );", [user_id, title, description])
+    return result
+  } catch (error) {
+    console.log(error);
+    return false
+  }
+}
+
+
+
+
+
+
 
 module.exports = {
   signupDB,
-  loginDB
+  loginDB,
+  createJobQueue
 }
